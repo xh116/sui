1b641f380c28d0fcc27e6cfc7de437bfb43d318c
xh116 <37950502+xh116@users.noreply.github.com>
2025-10-27 15:01:10 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",  
+  base: "./",
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:9090", // 或你本地的 Clash/Sing-box API 地址
+        target: "http://127.0.0.1:9090",  
         changeOrigin: true,
         ws: true,
         rewrite: path => path.replace(/^\/api/, "")
       }
     }
+  },
+  build: {
+    outDir: "sui"   i
   }
 });
